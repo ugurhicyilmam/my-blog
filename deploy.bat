70cfe393af638563138c80f198bfc52a73c300fa
ugur.hicyilmam <ugurhicyilmam@gmail.com>
2017-09-21 18:28:27 +0300

    remove console cleaning from deploy script

--- deploy.bat
+++ deploy.bat
@@ -4,26 +4,21 @@ set /P message=Commit Message:
 
 echo adding all files to git
 git add -A
-cls
 
 echo committing changes with message: %message%
 git commit -am "%message%"
-cls
 
 echo pushing changes
 git push
-cls
 
 echo building app
 hugo
-cls
 
 echo pushing changes
 cd public
 git add -A
 git commit -am "%message%"
 git push
-cls
 
 echo done :)
 
